3f3c7a22a09276f0563f05e6dab8e47e29d6a5e8
raza haider <raza.haider@maleyat.com>
2024-10-22 01:42:16 -0700

    add popup

--- src/app/components/ApplyDataGrid.tsx
+++ src/app/components/ApplyDataGrid.tsx
@@ -1,87 +1,121 @@
-
-"use client"
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { PostData } from "@/app/page";
-import {Rating, Link} from '@mui/material';
-
+import { Rating } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 type PostsTableProps = {
-    postsData: PostData[];
-  };
+  postsData: PostData[];
+};
 
-const columns: GridColDef[] = [
-    
-
-  { field: 'image', 
-    headerName: 'Image',
-     width: 150,
-     renderCell: (params) => (
-        <Link href={params.value} target="_blank" sx={{ width: 80, height: 80 }}>
-          <img
-            src={params.value}
-            alt="Product"
-            style={{ width: 80, objectFit: 'contain', height: "100%" }}
-          />
-        </Link>
-      ) 
-    
-    
+const getColumns = (openModal: (data: string) => void): GridColDef[] => {
+  return [
+    {
+      field: "image",
+      headerName: "Image",
+      width: 150,
+      renderCell: (params) => (
+        <img
+          onClick={() => openModal(params?.row?.image)}
+          src={params.value}
+          alt="Product"
+          style={{ width: 80, objectFit: "contain", height: "100%" }}
+        />
+      ),
     },
-  {
-    field: 'price',
-    headerName: 'Price',
-    width: 150,
-  },
-  {
-    field: 'title',
-    headerName: 'Title',
-    width: 150,
-  },
-  {
-    field: 'rating' ,
-    headerName: 'Rating',
-    type: 'number',
-    width: 110,
-    renderCell: (params) => (
-        <Rating value={params.value.rate} readOnly />
-      )
-
-    
-  },
-  {
-    field: 'description',
-    headerName: 'Description',
-    width: 200,
-  },
-];
+    {
+      field: "price",
+      headerName: "Price",
+      width: 150,
+    },
+    {
+      field: "title",
+      headerName: "Title",
+      width: 150,
+    },
+    {
+      field: "rating",
+      headerName: "Rating",
+      type: "number",
+      width: 110,
+      renderCell: (params) => <Rating value={params.value.rate} readOnly />,
+    },
+    {
+      field: "description",
+      headerName: "Description",
+      width: 200,
+    },
+  ];
+};
 
 export default function ApplyDataGrid({ postsData }: PostsTableProps) {
+  const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
+  const [data, setData] = React.useState<string>(""); 
 
-   
+
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleClickOpen = (data: string) => {
+    setIsModalOpen(true);
+    setData(data);
+  };
+
   return (
-
-    <Box sx={{ height: 600, width: '100%' }}>
-
-
-      <DataGrid
-         
-        rows={postsData}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
+    <React.Fragment>
+      <Box sx={{ height: 600, width: "100%" }}>
+        <DataGrid
+          rows={postsData}
+          columns={getColumns(handleClickOpen)}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
             },
-          },
-        }}
-        pageSizeOptions={[5]}
-        disableRowSelectionOnClick
-        rowHeight={120}
-        
-      />
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+          rowHeight={120}
+        />
+      </Box>
 
-    </Box>
+      <Dialog
+        open={isModalOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        
+      >
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={() => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: "red",
+          })}
+        >
+          <CloseIcon />
+        </IconButton>
+        <img
+          src={data}
+          alt="image"
+          style={{
+            width: "200px",
+            height: "200px",
+            margin: "auto",
+            padding: 20,
+          }}
+        />
+      </Dialog>
+    </React.Fragment>
   );
 }
